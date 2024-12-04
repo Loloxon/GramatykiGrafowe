9ac71bd6e3b2429a43e6fe082f0ef593f6c5adfe
Nikodem Korohoda <nikodemko@interia.pl>
2024-12-04 18:41:38 +0100

    improved example

--- main.py
+++ main.py
@@ -10,28 +10,37 @@ def sample_visualization():
              Node(1, 0, False, "V"),
              Node(1, 1, False, "V"),
              Node(0, 1, False, "V"),
+             Node(0, 2, False, "V"),
              Node(1, 2, False, "V"),
-             Node(0, 2, False, "V")]
+             Node(2, 2, False, "V"),
+             Node(2, 1, False, "V")]
     edges = [Edge(nodes[0], nodes[1], True),
              Edge(nodes[1], nodes[2], True),
              Edge(nodes[2], nodes[3], True),
              Edge(nodes[3], nodes[0], True),
-             Edge(nodes[2], nodes[4], True),
-             Edge(nodes[3], nodes[5], True),
-             Edge(nodes[4], nodes[5], True)]
-    hyperedges = [HyperEdge([nodes[0], nodes[1], nodes[2], nodes[3]], True)]
-
-    hypergraph_example = HyperGraph(nodes, edges, hyperedges)
-    nx_graph_example = hypergraph_example.parse_hypergraph_to_networkx()
-    HyperGraph.visualize_hypergraph(nx_graph_example, "Graph example",
-                                    False, False, False)
+             Edge(nodes[3], nodes[4], True),
+             Edge(nodes[4], nodes[5], True),
+             Edge(nodes[5], nodes[6], True),
+             Edge(nodes[6], nodes[7], True),
+             Edge(nodes[7], nodes[2], True),
+             Edge(nodes[2], nodes[5], True)]
+    hyperedges = [HyperEdge([nodes[0], nodes[1], nodes[2], nodes[3]], True),
+                  HyperEdge([nodes[5], nodes[6], nodes[7], nodes[2]], True)]
 
     p1 = P1()
     p1.visualize()
+
+    hypergraph_example = HyperGraph(nodes, edges, hyperedges)
+    nx_graph_example = hypergraph_example.parse_hypergraph_to_networkx()
+    HyperGraph.visualize_hypergraph(nx_graph_example, "Graph example", False, False, False)
+
     hypergraph_example = p1.apply(hypergraph_example)
     nx_graph_example = hypergraph_example.parse_hypergraph_to_networkx()
-    HyperGraph.visualize_hypergraph(nx_graph_example, "Graph example after P1",
-                                    False, False, False)
+    HyperGraph.visualize_hypergraph(nx_graph_example, "Graph example after P1", False, False, False)
+
+    hypergraph_example = p1.apply(hypergraph_example)
+    nx_graph_example = hypergraph_example.parse_hypergraph_to_networkx()
+    HyperGraph.visualize_hypergraph(nx_graph_example, "Graph example after P1 twice", False, False, False)
 
 
 if __name__ == "__main__":
