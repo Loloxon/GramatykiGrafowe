2da3e5b6131a3a3c5af554f2f7c8975bd365d1da
Jakub Strojewski <92330747+strjakub@users.noreply.github.com>
2024-12-06 16:20:31 +0100

    p2-implementation (#6)

--- productions/p2.py
+++ productions/p2.py
@@ -63,5 +63,34 @@ class P2(Production):
             hyperedges=hyperedges
         )
 
-    def apply(self, graph: HyperGraph) -> HyperGraph:
-        pass
+    def transform(self, graph: HyperGraph, node_map: dict[Node, Node], left_side: HyperGraph) -> (bool, HyperGraph):
+        node_1 = graph.split_edge(node_map[left_side.nodes[0]],
+                                  node_map[left_side.nodes[1]],
+                                  graph.get_edge_between(node_map[left_side.nodes[0]],
+                                                         node_map[left_side.nodes[1]]).is_border)
+        node_2 = node_map[left_side.nodes[4]]
+        node_3 = graph.split_edge(node_map[left_side.nodes[2]],
+                                  node_map[left_side.nodes[3]],
+                                  graph.get_edge_between(node_map[left_side.nodes[2]],
+                                                         node_map[left_side.nodes[3]]).is_border)
+        node_4 = graph.split_edge(node_map[left_side.nodes[3]],
+                                  node_map[left_side.nodes[0]],
+                                  graph.get_edge_between(node_map[left_side.nodes[3]],
+                                                         node_map[left_side.nodes[0]]).is_border)
+
+        graph.remove_hyperedge(list(filter(lambda x: not x.is_hanging, list(node_map.values()))))
+        node_center = graph.create_center_node(list(filter(lambda x: not x.is_hanging, list(node_map.values()))))
+
+        graph.add_edge(node_1, node_center)
+        graph.add_edge(node_2, node_center)
+        graph.add_edge(node_3, node_center)
+        graph.add_edge(node_4, node_center)
+
+        graph.add_hyperedge([node_1, node_2, node_map[left_side.nodes[1]], node_center])
+        graph.add_hyperedge([node_2, node_3, node_map[left_side.nodes[2]], node_center])
+        graph.add_hyperedge([node_3, node_4, node_map[left_side.nodes[3]], node_center])
+        graph.add_hyperedge([node_4, node_1, node_map[left_side.nodes[0]], node_center])
+
+        node_2.is_hanging = not node_2.is_hanging
+
+        return True, graph
