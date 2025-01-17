545992c3d3693b4ec468db40141b9eaf513e5587
Herdran <jakub4426@gmail.com>
2025-01-17 19:36:40 +0100

    Added predicates for P7 and P8, finished group 2 task

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ from graph.elements.hyperedge import HyperEdge
 from graph.elements.node import Node
 from graph.hypergraph import HyperGraph
 from productions.p1 import P1
+from productions.p11 import P11
+from productions.p17 import P17
 from productions.p2 import P2
+from productions.p3 import P3
+from productions.p7 import P7
+from productions.p8 import P8
 from utils import visualize_hypergraph
 
 
@@ -50,11 +55,90 @@ def sample_visualization():
     visualize_hypergraph(nx_graph_example, "Graph example after P1 and P2",
                          True, True, True)
 
+def group_2():
+    nodes = [Node(0, 0, False),
+             Node(1, 0, False),
+             Node(1, 0.5, False),
+             Node(1, 1, False),
+             Node(0, 1, False),
+
+             Node(0.3, 0.3, False),
+             Node(0.7, 0.3, False),
+             Node(0.85, 0.5, False),
+             Node(0.7, 0.7, False),
+             Node(0.3, 0.7, False)]
+    edges = [Edge(nodes[0], nodes[1], True),
+             Edge(nodes[1], nodes[2], True),
+             Edge(nodes[2], nodes[3], True),
+             Edge(nodes[3], nodes[4], True),
+             Edge(nodes[4], nodes[0], True),
+
+             Edge(nodes[5], nodes[6], False),
+             Edge(nodes[6], nodes[7], False),
+             Edge(nodes[7], nodes[8], False),
+             Edge(nodes[8], nodes[9], False),
+             Edge(nodes[9], nodes[5], False),
+
+             Edge(nodes[0], nodes[5], False),
+             Edge(nodes[1], nodes[6], False),
+             Edge(nodes[2], nodes[7], False),
+             Edge(nodes[3], nodes[8], False),
+             Edge(nodes[4], nodes[9], False)]
+    hyperedges = [HyperEdge([nodes[0], nodes[1], nodes[5], nodes[6]], False),
+                  HyperEdge([nodes[1], nodes[2], nodes[6], nodes[7]], False),
+                  HyperEdge([nodes[2], nodes[3], nodes[7], nodes[8]], False),
+                  HyperEdge([nodes[3], nodes[4], nodes[8], nodes[9]], False),
+                  HyperEdge([nodes[4], nodes[0], nodes[9], nodes[5]], False),
+
+                  HyperEdge([nodes[5], nodes[6], nodes[7], nodes[8], nodes[9]], False)]
+    hypergraph_example = HyperGraph(nodes, edges, hyperedges)
+
+    p1 = P1()
+    p2 = P2()
+    p7 = P7()
+    p8 = P8()
+    p3 = P3()
+    p11 = P11()
+    p17 = P17()
+
+    _, hypergraph_example = p7.apply_with_predicate(hypergraph_example)
+    nx_graph_example = hypergraph_example.parse_hypergraph_to_networkx()
+    visualize_hypergraph(nx_graph_example, "Graph 1",True, False, False)
+    _, hypergraph_example = p1.apply(hypergraph_example)
+    nx_graph_example = hypergraph_example.parse_hypergraph_to_networkx()
+    visualize_hypergraph(nx_graph_example, "Graph 2",True, False, False)
+    _, hypergraph_example = p7.apply_with_predicate(hypergraph_example)
+    nx_graph_example = hypergraph_example.parse_hypergraph_to_networkx()
+    visualize_hypergraph(nx_graph_example, "Graph 3",True, False, False)
+    _, hypergraph_example = p8.apply_with_predicate(hypergraph_example)
+    _, hypergraph_example = p17.apply(hypergraph_example)
+    nx_graph_example = hypergraph_example.parse_hypergraph_to_networkx()
+    visualize_hypergraph(nx_graph_example, "Graph 4",True, False, False)
     _, hypergraph_example = p2.apply(hypergraph_example)
     nx_graph_example = hypergraph_example.parse_hypergraph_to_networkx()
-    visualize_hypergraph(nx_graph_example, "Graph example after P1 and 2*P2",
-                         False, False, False)
-
+    visualize_hypergraph(nx_graph_example, "Graph 5",True, False, False)
+    _, hypergraph_example = p11.apply(hypergraph_example)
+    nx_graph_example = hypergraph_example.parse_hypergraph_to_networkx()
+    visualize_hypergraph(nx_graph_example, "Graph 6",True, False, False)
+    _, hypergraph_example = p1.apply(hypergraph_example)
+    nx_graph_example = hypergraph_example.parse_hypergraph_to_networkx()
+    visualize_hypergraph(nx_graph_example, "Graph 7",True, False, False)
+    _, hypergraph_example = p7.apply_with_predicate(hypergraph_example)
+    nx_graph_example = hypergraph_example.parse_hypergraph_to_networkx()
+    visualize_hypergraph(nx_graph_example, "Graph 8",True, False, False)
+    _, hypergraph_example = p8.apply_with_predicate(hypergraph_example)
+    _, hypergraph_example = p8.apply_with_predicate(hypergraph_example)
+    nx_graph_example = hypergraph_example.parse_hypergraph_to_networkx()
+    visualize_hypergraph(nx_graph_example, "Graph 9",True, False, False)
+    _, hypergraph_example = p2.apply(hypergraph_example)
+    nx_graph_example = hypergraph_example.parse_hypergraph_to_networkx()
+    visualize_hypergraph(nx_graph_example, "Graph 10",True, False, False)
+    _, hypergraph_example = p3.apply(hypergraph_example)
+    nx_graph_example = hypergraph_example.parse_hypergraph_to_networkx()
+    visualize_hypergraph(nx_graph_example, "Graph 11",True, False, False)
+    _, hypergraph_example = p1.apply(hypergraph_example)
+    nx_graph_example = hypergraph_example.parse_hypergraph_to_networkx()
+    visualize_hypergraph(nx_graph_example, "Graph 12",True, False, False)
 
 if __name__ == "__main__":
-    sample_visualization()
+    group_2()
--- productions/p7.py
+++ productions/p7.py
@@ -1,8 +1,8 @@
-from graph.elements.edge import Edge
 from graph.elements.hyperedge import HyperEdge
 from graph.elements.node import Node
 from graph.hypergraph import HyperGraph
 from productions.production import Production
+from utils import find_subgraphs
 
 
 class P7(Production):
@@ -44,8 +44,35 @@ class P7(Production):
         node_3 = node_map[left_side.nodes[2]]
         node_4 = node_map[left_side.nodes[3]]
 
-        graph.remove_hyperedge(list(filter(lambda x: not x.is_hanging, list(node_map.values()))))
+        graph.remove_hyperedge(list(node_map.values()))
 
         graph.add_hyperedge([node_1, node_2, node_3, node_4], True)
 
         return True, graph
+
+    def apply_with_predicate(self, graph: HyperGraph) -> (bool, HyperGraph):
+        left_side = self.get_left_side()
+        node_maps = find_subgraphs(graph, left_side)
+
+        for node_map in node_maps:
+            if predicate(node_map):
+                reversed_node_map = {v: k for k, v in node_map.items() if isinstance(v, Node) and isinstance(k, Node)}
+                return self.transform(graph, reversed_node_map, left_side)
+
+        return False, graph
+
+
+def predicate(node_map):
+    has_correct_node = False
+    has_wrong_node = False
+    for node in node_map.keys():
+        if isinstance(node, Node):
+            if node.x == 0.3 and node.y == 0.7:
+                has_wrong_node = True
+            elif node.x == 0.5 and node.y == 0.7:
+                has_wrong_node = True
+            elif node.x == 0.3 and node.y == 0.3:
+                has_wrong_node = True
+            elif node.x == 0.7 and node.y == 0.7:
+                has_correct_node = True
+    return has_correct_node and not has_wrong_node
