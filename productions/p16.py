2784e0d5ad281b2e18f3a6f83400c240db35c24e
Piotr Czarnik <ptr.czarnik@gmail.com>
2025-01-24 13:33:28 +0100

    Fix P16

--- productions/p16.py
+++ productions/p16.py
@@ -57,6 +57,14 @@ class P16(Production):
         )
 
     def transform(self, graph: HyperGraph, node_map: dict[Node, Node], left_side: HyperGraph) -> (bool, HyperGraph):
-        graph.hyperedges[0].is_removable = not graph.hyperedges[0].is_removable
+        node_1 = node_map[left_side.nodes[0]]
+        node_2 = node_map[left_side.nodes[1]]
+        node_3 = node_map[left_side.nodes[2]]
+        node_4 = node_map[left_side.nodes[3]]
+        node_5 = node_map[left_side.nodes[4]]
+
+        graph.remove_hyperedge(list(filter(lambda x: not x.is_hanging, list(node_map.values()))))
+
+        graph.add_hyperedge([node_1, node_2, node_3, node_4, node_5], True, label="P")
 
         return True, graph
